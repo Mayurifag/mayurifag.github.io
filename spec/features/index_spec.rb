14072810f68030e93262b1e6fe876e44d75723ec
cvsgbhn <kbh123456g@gmail.com>
2019-10-08 00:27:41 +0300

    Add job position feature spec

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -12,4 +12,8 @@ describe 'Main page', type: :feature do
   it 'has developer name in text' do
     expect(page).to have_text 'Vladislav Ponomarev'
   end
+
+  it 'has job position in text' do
+    expect(page).to have_text 'Team Lead'
+  end
 end
